02d0d93b5dc615fdf1b43142c73401bdbab431da
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-06-03 00:39:48 -0500

    added homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,10 @@
+import Homepage from "@/components/Homepage";
+
 export default function Home() {
   return (
-    <main className="flex flex-col">
-      <div>
-        <h1>Seu próximo desenvolvedor</h1>
-        <h1>Fullstack ;)</h1>
-        <p>Me chamo Gabriel Feitosa e sou desenvolvedor fullstack React, Node e React Native, saiba mais sobre mim.</p>
-      </div>
-      
-      <div>
-        <button>Linked In</button>
-        <button>Github</button>
-      </div>
-    </main>
+    <>
+      <Homepage />
+    </>
   )
 }
  
