065e67cdd4cb1eb09fc436d9a32ded5d3bf66f7b
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-06-05 16:11:29 -0500

    Added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,27 @@
-import Link from "next/link";
+import Link from "next/link"
+import { Outfit } from "next/font/google"
+
+const outfit = Outfit({
+  subsets: ['latin']
+})
 
 export default function Header() {
     return (
-        <header>
-            <p>
-                {"<Gabriel.dev />"}
-            </p>
+        <header className="w-screen flex flex-row justify-center">
+            <div className="w-full max-w-7xl px-4 py-7 flex flex-row justify-between items-center">
+                <p className={`${outfit.className} text-2xl bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-violet-500`}>
+                    {"<Gabriel.dev />"}
+                </p>
 
-            <nav>
-                <ul>
-                    <li><Link href="">Home</Link></li>
-                    <li><Link href="">Sobre mim</Link></li>
-                    <li><Link href="">Projetos</Link></li>
-                    <li><Link href="">Contatos</Link></li>
-                </ul>
-            </nav>
+                <nav>
+                    <ul className="flex flex-row gap-8 font-semibold text-slate-700">
+                        <li><Link href="">Home</Link></li>
+                        <li><Link href="">Sobre mim</Link></li>
+                        <li><Link href="">Projetos</Link></li>
+                        <li><Link href="">Contatos</Link></li>
+                    </ul>
+                </nav>
+            </div>
         </header>
     )
 }
