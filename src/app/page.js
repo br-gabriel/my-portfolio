ec8c9e0a2d8fc2ee333f023e2042c31e206c58f4
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-06-05 21:35:49 -0500

    added components

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
-import Homepage from "@/components/Homepage";
+import Homepage from "@/components/Homepage"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
-    <>
+    <main className="w-screen flex flex-col justify-between items-center relative bg-gradient-to-br from-violet-200 via-white to-sky-100">
+      <Header />
       <Homepage />
-    </>
+      <Footer />
+    </main>
   )
 }
  
