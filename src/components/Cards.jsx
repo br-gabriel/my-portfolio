c372a79217d0334fbdcedbc0439f26e4b3c10877
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-06-07 18:52:53 -0500

    card session turned into component

---
+++ src/components/Cards.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import mobileSVG from "../../public/mobile.svg"
+import nextjsSVG from "../../public/nextjs.svg"
+import figmaSVG from "../../public/figma.svg"
+
+export default function Cards() {
+    return (
+        <section className="max-w-7xl w-full flex flex-row items-center justify-center gap-4 mt-4 px-4">
+            <div className="min-w-[350px] h-36 bg-white/90 drop-shadow-md rounded-md flex flex-row items-center justify-center gap-5">
+                <Image src={figmaSVG} width={50} />
+                <span className="max-w-[150px]">Criação de design feito com Figma</span>
+            </div>
+            <div className="min-w-[350px] h-36 bg-white/90 drop-shadow-md rounded-md flex flex-row items-center justify-center gap-5">
+                <Image src={nextjsSVG} width={70} />
+                <span className="max-w-[150px]">Sites performáticos utilizando NextJS</span>
+            </div>
+            <div className="min-w-[350px] h-36 bg-white/90 drop-shadow-md rounded-md flex flex-row items-center justify-center gap-5">
+                <Image src={mobileSVG} width={50} />
+                <span className="max-w-[150px]">Sites responsivos</span>
+            </div>
+        </section>
+    )
+}
