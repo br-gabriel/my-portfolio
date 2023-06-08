351f74934a794ff819b580842d90d09a0e79f2a6
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-06-08 17:04:50 -0500

    background update

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import Homepage from "@/components/Homepage"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
+import Aboutme from "@/components/AboutMe"
+import Projects from "@/components/Projects"
 
 export default function Home() {
   return (
-    <main className="w-screen flex flex-col justify-between items-center relative bg-gradient-to-br from-violet-200 via-white to-sky-100">
+    <main className="w-full flex flex-col justify-between items-center relative bg-gradient-to-br from-[#E4E6FF] via-white to-[#E4EBFF]">
       <Header />
       <Homepage />
+      <Aboutme />
+      <Projects />
       <Footer />
     </main>
   )
