55c3d443949899d527d1c00c9580048fe60c21fe
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-06-15 23:38:14 -0500

    carrossel update

--- src/components/CarouselProjects.jsx
+++ src/components/CarouselProjects.jsx
@@ -1,11 +1,9 @@
 "use client"
+import Link from 'next/link'
+import Image from 'next/image'
+import { cards } from '../ProjectsData.json'
 import { register } from 'swiper/element/bundle'
 import { Outfit } from 'next/font/google'
-import Image from 'next/image'
-import criptoTrackerBanner from '../../public/images/criptotracker.png'
-import habitsBanner from '../../public/images/habits.png'
-import finansBanner from '../../public/images/finans.png'
-import dtmoneyBanner from '../../public/images/dtmoney.png'
 import { FaGithub } from "react-icons/fa"
 import { GoLinkExternal } from "react-icons/go"
 
@@ -16,136 +14,68 @@ const outfit = Outfit({
 register();
 
 export default function Carousel() {
-  return (
-    <div className='max-w-7xl w-full h-full flex flex-row justify-center items-center'>
-        <swiper-container
-            slides-per-view="1"
-            navigation="true"
-            pagination="false"
-            loop="true"
-            autoplay="true"
-            speed="500"
-        >
-            <swiper-slide>
-                  <div className='w-full h-[500px] flex flex-row items-center justify-center'>
-                      <div className='max-w-[1150px] w-full h-full flex flex-row'>
-                          <Image src={criptoTrackerBanner} alt='banner do app cripto tracker' className='w-full h-full rounded-l-lg' />
+    return (
+        <div className='w-[1120px] h-[500px] flex items-center justify-center'>
+            <swiper-container
+                space-between="-90"
+                slides-per-view="2"
+                navigation="true"
+                pagination="false"
+                loop="true"
+                autoplay="true"
+                speed="500"
+            >
+                {cards.map((card) => (
+                    <swiper-slide key={card.title}>
+                        <div className='flex justify-center'>
+                            <div className='flex flex-col items-center h-[500px] w-[26em] p-4 mb-2 rounded-lg bg-white drop-shadow-md relative'>
+                                <Image src={card.image.url} alt={card.image.alt} width={400} height={320} className='rounded-lg'/>
+                                
+                                <div className='bg-white pt-4 h-full flex flex-col justify-between items-center'>
+                                    <div>
+                                        <h3 className={`${outfit.className} text-blue-950 font-semibold text-xl text-center mb-4`}>
+                                            {card.title}
+                                        </h3>
 
-                          <div className='max-w-[350px] w-full h-full bg-white p-4 rounded-r-lg flex flex-col items-center justify-between'>
-                              <div className='mt-4'>
-                                  <h4 className={`${outfit.className} bold mb-4 text-2xl text-blue-950 text-center`}>
-                                      Cripto Tracker App
-                                  </h4>
-                                  <span>
-                                      Aplicativo criado com React Native que mostra as principais informações das cripto moedas lançadas no mercado...
-                                  </span>
-                              </div>
+                                        <div className='max-h-[5em] w-[100%] overflow-y-auto'>
+                                            <span>
+                                                {card.description}
+                                            </span>
+                                        </div>
+                                    </div>
 
-                              <div className="flex gap-2 mb-12">
-                                  <button className="w-36 flex flex-row justify-center items-center gap-3 bg-slate-500 text-slate-50 px-3 py-1 rounded  drop-shadow">
-                                      <FaGithub size={19} />
-                                      Github
-                                  </button>
-                                  <button className="w-36 flex flex-row justify-center items-center gap-3 bg-white text-slate-500 px-3 py-1 rounded border border-slate-500 drop-shadow">
-                                      <GoLinkExternal size={19} />
-                                      Live demo
-                                  </button>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-            </swiper-slide>
+                                    {card.links.secondLink === "" ? 
+                                        <div className='flex flex-row items-center justify-between w-[100%] mt-4'>
+                                            <Link href={card.links.firstLink} target='blank' className='w-full'>
+                                                <button className='w-full flex flex-row justify-center items-center gap-3 bg-slate-500 border border-slate-500  text-slate-50 px-3 py-1 rounded drop-shadow duration-200 hover:bg-slate-500/80 hover:ease-in-out hover:scale-105'>
+                                                    <FaGithub size={19} />
+                                                    Github
+                                                </button>
+                                            </Link>
+                                        </div>
+                                    :
+                                        <div className='flex flex-row items-center justify-center gap-6 w-[100%] mt-4'>
+                                            <Link href={card.links.firstLink} target='blank'>
+                                                <button className='w-40 flex flex-row justify-center items-center gap-3 bg-slate-500 border border-slate-500  text-slate-50 px-3 py-1 rounded drop-shadow duration-200 hover:bg-slate-500/80 hover:ease-in-out hover:scale-105'>
+                                                    <FaGithub size={19} />
+                                                    Github
+                                                </button>
+                                            </Link>
 
-            <swiper-slide>
-                  <div className='w-full h-[500px] flex flex-row items-center justify-center'>
-                      <div className='max-w-[1150px] w-full h-full flex flex-row'>
-                          <Image src={habitsBanner} alt='banner do app de hábitos' className='w-full h-full rounded-l-lg' />
-
-                          <div className='max-w-[350px] w-full h-full bg-white p-4 rounded-r-lg flex flex-col items-center justify-between'>
-                              <div className='mt-4'>
-                                  <h4 className={`${outfit.className} bold mb-4 text-2xl text-blue-950 text-center`}>
-                                      Habit App
-                                  </h4>
-                                  <span>
-                                      Aplicativo criado com React Native que mostra as principais informações das cripto moedas lançadas no mercado...
-                                  </span>
-                              </div>
-
-                              <div className="flex gap-2 mb-12">
-                                  <button className="w-36 flex flex-row justify-center items-center gap-3 bg-slate-500 text-slate-50 px-3 py-1 rounded  drop-shadow">
-                                      <FaGithub size={19} />
-                                      Github
-                                  </button>
-                                  <button className="w-36 flex flex-row justify-center items-center gap-3 bg-white text-slate-500 px-3 py-1 rounded border border-slate-500 drop-shadow">
-                                      <GoLinkExternal size={19} />
-                                      Live demo
-                                  </button>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-            </swiper-slide>
-
-            <swiper-slide>
-                  <div className='w-full h-[500px] flex flex-row items-center justify-center'>
-                      <div className='max-w-[1150px] w-full h-full flex flex-row'>
-                          <Image src={finansBanner} alt='banner do site finans - instituição financeira fictícia' className='w-full h-full rounded-l-lg' />
-
-                          <div className='max-w-[350px] w-full h-full bg-white p-4 rounded-r-lg flex flex-col items-center justify-between'>
-                              <div className='mt-4'>
-                                  <h4 className={`${outfit.className} bold mb-4 text-2xl text-blue-950 text-center`}>
-                                      Site Finans
-                                  </h4>
-                                  <span>
-                                      Aplicativo criado com React Native que mostra as principais informações das cripto moedas lançadas no mercado...
-                                  </span>
-                              </div>
-
-                              <div className="flex gap-2 mb-12">
-                                  <button className="w-36 flex flex-row justify-center items-center gap-3 bg-slate-500 text-slate-50 px-3 py-1 rounded  drop-shadow">
-                                      <FaGithub size={19} />
-                                      Github
-                                  </button>
-                                  <button className="w-36 flex flex-row justify-center items-center gap-3 bg-white text-slate-500 px-3 py-1 rounded border border-slate-500 drop-shadow">
-                                      <GoLinkExternal size={19} />
-                                      Live demo
-                                  </button>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-            </swiper-slide>
-
-            <swiper-slide>
-                  <div className='w-full h-[500px] flex flex-row items-center justify-center'>
-                      <div className='max-w-[1150px] w-full h-full flex flex-row'>
-                          <Image src={dtmoneyBanner} alt='banner do site dt money - gestor financeiro' className='w-full h-full rounded-l-lg' />
-
-                          <div className='max-w-[350px] w-full h-full bg-white p-4 rounded-r-lg flex flex-col items-center justify-between'>
-                              <div className='mt-4'>
-                                  <h4 className={`${outfit.className} bold mb-4 text-2xl text-blue-950 text-center`}>
-                                      Dt Money
-                                  </h4>
-                                  <span>
-                                      Aplicativo criado com React Native que mostra as principais informações das cripto moedas lançadas no mercado...
-                                  </span>
-                              </div>
-
-                              <div className="flex gap-2 mb-12">
-                                  <button className="w-36 flex flex-row justify-center items-center gap-3 bg-slate-500 text-slate-50 px-3 py-1 rounded  drop-shadow">
-                                      <FaGithub size={19} />
-                                      Github
-                                  </button>
-                                  <button className="w-36 flex flex-row justify-center items-center gap-3 bg-white text-slate-500 px-3 py-1 rounded border border-slate-500 drop-shadow">
-                                      <GoLinkExternal size={19} />
-                                      Live demo
-                                  </button>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-            </swiper-slide>
-        </swiper-container>
-    </div>
+                                            <Link href={card.links.secondLink} target='blank'>
+                                                <button className='w-40 flex flex-row justify-center items-center gap-3 bg-white text-slate-500 px-3 py-1 rounded border border-slate-500 drop-shadow duration-200 hover:text-slate-500/80 hover:border-slate-500/80 hover:ease-in-out hover:scale-105'>
+                                                    <GoLinkExternal size={19} />
+                                                    Live demo
+                                                </button>
+                                            </Link>
+                                        </div>
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    </swiper-slide>
+                ))}
+            </swiper-container>
+        </div>
   );
 };
