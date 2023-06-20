e1acdf0cc943ddf993d685f87e60e357cb866ce1
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-06-19 22:56:25 -0500

    responsive design update

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -24,35 +24,39 @@ export default function Cards() {
     }, []);
 
     return (
-        <section className="max-w-7xl w-full flex flex-row items-center justify-center gap-4 mt-12 px-4 py-3">
+        <section className="max-w-7xl w-full flex flex-row items-center justify-center gap-4 px-4 py-3">
             {console.log(largura)}
             <swiper-container
                 space-between="10"
-                slides-per-view={largura > 800 ? 3 : 1}
+                slides-per-view={largura > 900 ? 3 : 1}
                 loop="true"
                 autoplay="true"
                 speed="500"
             >
                 <swiper-slide>
-                    <div className="h-40">
-                        <div className="min-w-[350px] md:min-w-[250px] h-36 bg-white drop-shadow-md rounded-md flex flex-row items-center justify-center gap-5">
-                            <Image src={figmaSVG} width={50} />
+                    <div className="h-40 p-2">
+                        <div className="min-w-[150px] md:min-w-[250px] h-36 bg-white drop-shadow-md rounded-md flex flex-row items-center justify-center gap-5">
+                            <Image src={figmaSVG} alt="logo do Figma" width={50} />
                             <span className="max-w-[150px]">Criação de design feito com Figma</span>
                         </div>
                     </div>
                 </swiper-slide>
 
                 <swiper-slide>
-                    <div className="min-w-[350px] md:min-w-[250px] h-36 bg-white drop-shadow-md rounded-md flex flex-row items-center justify-center gap-5">
-                        <Image src={nextjsSVG} width={70} />
-                        <span className="max-w-[150px]">Sites performáticos utilizando NextJS</span>
+                    <div className="h-40 p-2">
+                        <div className="min-w-[150px] md:min-w-[250px] h-36 bg-white drop-shadow-md rounded-md flex flex-row items-center justify-center gap-5">
+                            <Image src={nextjsSVG} alt="logo do Next js" width={70} />
+                            <span className="max-w-[150px]">Sites performáticos utilizando NextJS</span>
+                        </div>
                     </div>
                 </swiper-slide>
 
                 <swiper-slide>
-                    <div className="min-w-[350px] md:min-w-[250px] sm:min-w-[150px] h-36 bg-white drop-shadow-md rounded-md flex flex-row items-center justify-center gap-5">
-                        <Image src={mobileSVG} width={50} />
-                        <span className="max-w-[150px]">Sites responsivos</span>
+                    <div className="h-40 p-2">
+                        <div className="min-w-[150px] md:min-w-[250px] h-36 bg-white drop-shadow-md rounded-md flex flex-row items-center justify-center gap-5">
+                            <Image src={mobileSVG} alt="icone de smartphone" width={50} />
+                            <span className="max-w-[150px]">Sites responsivos</span>
+                        </div>
                     </div>
                 </swiper-slide>
             </swiper-container>
