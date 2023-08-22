96b1f0ae35962539096040d15b4091d8f5d216fa
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-08-21 20:04:19 -0500

    responsive update

--- src/components/Aboutme/Cards.jsx
+++ src/components/Aboutme/Cards.jsx
@@ -1,29 +1,29 @@
 import Image from "next/image"
-import mobileSVG from "../../../public/mobile.svg"
-import nextjsSVG from "../../../public/nextjs.svg"
 import figmaSVG from "../../../public/figma.svg"
+import nextjsSVG from "../../../public/nextjs.svg"
+import mobileSVG from "../../../public/mobile.svg"
 
 export default function Cards() {
     return (
-        <section className="max-w-7xl w-full flex flex-col md:flex-row items-center justify-center gap-2 px-4 py-3">
-            <div className="h-40 w-full p-2">
-                <div className="min-w-[150px] md:min-w-[230px] h-36 bg-white dark:bg-black/80 dark:text-slate-50 drop-shadow-md rounded-md flex flex-row items-center justify-center gap-5">
-                    <Image src={figmaSVG} alt="logo do Figma" width={50} />
-                    <span className="max-w-[150px]">Criação de design feito com Figma</span>
-                </div>
-            </div>
+        <section dra className="w-full flex md:justify-center md:items-center gap-4 p-4 snap-mandatory snap-x overflow-x-scroll md:overflow-hidden drop-shadow-md scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-800">
+            <div className="bg-white dark:bg-black text-black dark:text-white rounded-md p-4 w-full min-w-[270px] max-w-[300px] min-h-[120px] flex items-center justify-center snap-center">
+                <div className="flex items-center justify-center gap-4">
+                     <Image src={figmaSVG} alt="logo do Figma" width={50} />
+                     <span>Criação de design feito com Figma</span>
+                 </div>
+             </div>
 
-            <div className="h-40 w-full p-2">
-                <div className="min-w-[150px] md:min-w-[230px] h-36 bg-white dark:bg-black/80 dark:text-slate-50 drop-shadow-md rounded-md flex flex-row items-center justify-center gap-5">
+            <div className="bg-white dark:bg-black text-black dark:text-white rounded-md p-4 w-full min-w-[270px] max-w-[300px] min-h-[120px] flex items-center justify-center snap-center">
+                <div className="flex items-center justify-center gap-4">
                     <Image src={nextjsSVG} alt="logo do Next js" width={70} />
-                    <span className="max-w-[150px]">Sites performáticos utilizando NextJS</span>
+                    <span>Sites performáticos utilizando NextJS</span>
                 </div>
             </div>
 
-            <div className="h-40 w-full p-2">
-                <div className="min-w-[150px] md:min-w-[230px] h-36 bg-white dark:bg-black/80 dark:text-slate-50 drop-shadow-md rounded-md flex flex-row items-center justify-center gap-5">
-                    <Image src={mobileSVG} alt="icone de smartphone" width={50} />
-                    <span className="max-w-[150px]">Sites responsivos</span>
+            <div className="bg-white dark:bg-black text-black dark:text-white rounded-md p-4 w-full min-w-[270px] max-w-[300px] min-h-[120px] flex items-center justify-center snap-center">
+                <div className="flex items-center justify-center gap-4">
+                    <Image src={mobileSVG} alt="Icone de smartphone" width={50} />
+                    <span>Sites responsivos</span>
                 </div>
             </div>
         </section>
