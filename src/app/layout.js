662785bd84cc11f53966b23528d746499382db46
Gabriel Feitosa <contato.gabrielfeitosa@gmail.com>
2023-08-21 20:05:16 -0500

    custom scrollbar added

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR" id="myHtml">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} scrollbar-thin scrollbar-track-white dark:scrollbar-track-black scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-800`}>{children}</body>
     </html>
   )
 }
