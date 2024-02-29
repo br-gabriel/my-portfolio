22889346ef854406b8154bcb84c9e1b50289a625
gabriel <contato.gabrielfeitosa@gmail.com>
2024-02-28 21:53:39 -0300

    overflow fixed

--- src/components/Aboutme/Cards.jsx
+++ src/components/Aboutme/Cards.jsx
@@ -5,7 +5,7 @@ import mobileSVG from "../../../public/mobile.svg"
 
 export default function Cards() {
   return (
-    <section dra className="w-full flex md:justify-center md:items-center gap-4 p-4 snap-mandatory snap-x overflow-x-scroll drop-shadow-md scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-800">
+    <section dra className="w-full flex md:justify-center md:items-center gap-4 p-4 snap-mandatory snap-x overflow-x-scroll md:overflow-hidden drop-shadow-md scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-800">
       <div className="bg-white dark:bg-black text-black dark:text-white rounded-md p-4 w-full min-w-[270px] max-w-[300px] md:max-w-[360px] min-h-[120px] flex items-center justify-center snap-center">
         <div className="flex items-center justify-center gap-4">
           <Image src={figmaSVG} alt="logo do Figma" width={50} />
