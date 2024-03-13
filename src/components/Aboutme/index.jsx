5f3addcf2812e1d3651e7c1f00fb5dadb3eb9b7b
gabriel <contato.gabrielfeitosa@gmail.com>
2024-03-13 15:07:36 -0300

    margin top changed

--- src/components/Aboutme/index.jsx
+++ src/components/Aboutme/index.jsx
@@ -12,7 +12,7 @@ const outfit = Outfit({
 export default function Aboutme() {   
   return (
     <div id="aboutMe" className="w-full h-full flex flex-col items-center">
-      <section className="bg-white dark:bg-black/60 w-full h-full md:h-[55vh] mt-0 md:mt-36 flex flex-col items-center justify-center drop-shadow">
+      <section className="bg-white dark:bg-black/60 w-full h-full md:h-[55vh] mt-0 md:mt-28 flex flex-col items-center justify-center drop-shadow">
         <div className="max-w-7xl w-full px-4 py-8 flex flex-col md:flex-row items-center justify-center gap-12">
           <div className="w-[235px] md:min-w-[335px] dark:hidden">
             <Image src={profilePicLight} alt="foto de perfil" width={335} />
