7e7426230d2f3c6596f15432fc0bbe95fd8c6660
gabriel <contato.gabrielfeitosa@gmail.com>
2024-06-18 13:09:33 -0500

    footer animation update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,6 +23,7 @@ export default function Footer() {
         <motion.h3 
         initial={{ opacity: 0, y: -50 }}
         whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.15 }}
         className={`${outfit.className} text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-violet-500 to-violet-500`}
         >
           Vamos trabalhar juntos!
@@ -30,9 +31,9 @@ export default function Footer() {
 
         <ul className="text-white mt-6 ml-4 flex flex-col gap-2">
           <motion.li 
-          initial={{ opacity: 0, y: -50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.3 }}
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.15, delay: 0.2 }}
           className="flex flex-row gap-4"
           >
             <Link href="https://wa.me/5568992490473" target="blank" className="flex justify-center items-center gap-2 hover:text-blue-500 transition ease-in-out">
@@ -42,9 +43,9 @@ export default function Footer() {
             </Link>
           </motion.li>
           <motion.li 
-          initial={{ opacity: 0, y: -50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.6 }}
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.15, delay: 0.3 }}
           className="flex flex-row gap-4"
           >
             <button onClick={handleCopyText} className="flex justify-center items-center gap-2 hover:text-blue-500 transition ease-in-out">
@@ -54,9 +55,9 @@ export default function Footer() {
             </button>
           </motion.li>
           <motion.li 
-          initial={{ opacity: 0, y: -50 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{ delay: 0.9 }}
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.15, delay: 0.4 }}
           className="flex flex-row gap-4"
           >
             <Link href="https://www.linkedin.com/in/gabriel-silva-feitosa/" target="blank" className="flex justify-center items-center gap-2 hover:text-blue-500 transition ease-in-out">
