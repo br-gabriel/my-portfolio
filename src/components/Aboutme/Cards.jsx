6cfa1809a5c7df12be020da0f6bc6e2f987ae00c
gabriel <contato.gabrielfeitosa@gmail.com>
2024-10-02 00:15:21 -0500

    animation update

--- src/components/Aboutme/Cards.jsx
+++ src/components/Aboutme/Cards.jsx
@@ -1,53 +1,62 @@
-"use client"
-import Image from "next/image"
-import figmaSVG from "../../../public/figma.svg"
-import nextjsSVG from "../../../public/nextjs.svg"
-import mobileSVG from "../../../public/mobile.svg"
-import { motion } from "framer-motion"
+"use client";
+import Image from "next/image";
+import figmaSVG from "../../../public/figma.svg";
+import nextjsSVG from "../../../public/nextjs.svg";
+import mobileSVG from "../../../public/mobile.svg";
+import { LayoutGroup, motion } from "framer-motion";
 
 export default function Cards() {
   const baseDelay = 0.2;
-  
+
   return (
-    <section dra className="will-change-transform w-full flex md:justify-center md:items-center gap-4 p-4 snap-mandatory snap-x overflow-x-scroll md:overflow-hidden drop-shadow-md scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-800">
-      <motion.div 
-        initial={{ opacity: 0, x: -100 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.3, delay: baseDelay * 6 }}
-        className="bg-white dark:bg-black text-black dark:text-white rounded-md p-4 w-full min-w-[270px] max-w-[300px] md:max-w-[360px] min-h-[120px] flex items-center justify-center snap-center"
-      >
-        <div className="flex items-center justify-center gap-6">
-          <Image src={figmaSVG} alt="logo do Figma" width={50} />
-          <span className="md:max-w-[150px]">Criação de design feito com Figma</span>
-        </div>
-      </motion.div>
+    <section
+      dra
+      className="flex w-full snap-x snap-mandatory gap-4 overflow-x-scroll p-4 drop-shadow-md will-change-transform scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-800 md:items-center md:justify-center md:overflow-hidden"
+    >
+      <LayoutGroup>
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.3, delay: baseDelay * 6 }}
+          className="flex min-h-[120px] w-full min-w-[270px] max-w-[300px] snap-center items-center justify-center rounded-md bg-white p-4 text-black dark:bg-black dark:text-white md:max-w-[360px]"
+        >
+          <div className="flex items-center justify-center gap-6">
+            <Image src={figmaSVG} alt="logo do Figma" width={50} />
+            <span className="md:max-w-[150px]">
+              Criação de design feito com Figma
+            </span>
+          </div>
+        </motion.div>
 
-      <motion.div 
-      initial={{ opacity: 0, x: -100 }}
-      whileInView={{ opacity: 1, x: 0 }}
-      viewport={{ once: true }}
-      transition={{ duration: 0.3, delay: baseDelay * 7 }}
-        className="bg-white dark:bg-black text-black dark:text-white rounded-md p-4 w-full min-w-[270px] max-w-[300px] md:max-w-[360px] min-h-[120px] flex items-center justify-center snap-center"
-      >
-        <div className="flex items-center justify-center gap-6">
-          <Image src={nextjsSVG} alt="logo do Next js" width={70} />
-          <span className="md:max-w-[150px]">Sites performáticos utilizando NextJS</span>
-        </div>
-      </motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.3, delay: baseDelay * 7 }}
+          className="flex min-h-[120px] w-full min-w-[270px] max-w-[300px] snap-center items-center justify-center rounded-md bg-white p-4 text-black dark:bg-black dark:text-white md:max-w-[360px]"
+        >
+          <div className="flex items-center justify-center gap-6">
+            <Image src={nextjsSVG} alt="logo do Next js" width={70} />
+            <span className="md:max-w-[150px]">
+              Sites performáticos utilizando NextJS
+            </span>
+          </div>
+        </motion.div>
 
-      <motion.div 
-      initial={{ opacity: 0, x: -100 }}
-      whileInView={{ opacity: 1, x: 0 }}
-      viewport={{ once: true }}
-      transition={{ duration: 0.3, delay: baseDelay * 8 }}
-        className="bg-white dark:bg-black text-black dark:text-white rounded-md p-4 w-full min-w-[270px] max-w-[300px] md:max-w-[360px] min-h-[120px] flex items-center justify-center snap-center"
-      >
-        <div className="flex items-center justify-center gap-6">
-          <Image src={mobileSVG} alt="Icone de smartphone" width={50} />
-          <span className="md:max-w-[150px]">Sites responsivos</span>
-        </div>
-      </motion.div>
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.3, delay: baseDelay * 8 }}
+          className="flex min-h-[120px] w-full min-w-[270px] max-w-[300px] snap-center items-center justify-center rounded-md bg-white p-4 text-black dark:bg-black dark:text-white md:max-w-[360px]"
+        >
+          <div className="flex items-center justify-center gap-6">
+            <Image src={mobileSVG} alt="Icone de smartphone" width={50} />
+            <span className="md:max-w-[150px]">Sites responsivos</span>
+          </div>
+        </motion.div>
+      </LayoutGroup>
     </section>
-  )
+  );
 }
--- src/components/Aboutme/index.jsx
+++ src/components/Aboutme/index.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import profilePicLight from "../../../public/images/lightmode-avatar.png";
 import profilePicDark from "../../../public/images/darkmode-avatar.png";
-import { motion } from "framer-motion";
+import { LayoutGroup, motion } from "framer-motion";
 import { Outfit } from "next/font/google";
 import Cards from "./Cards";
 
@@ -13,83 +13,87 @@ const outfit = Outfit({
 
 export default function Aboutme() {
   const baseDelay = 0.2;
-  
+
   return (
     <div id="aboutMe" className="flex h-full w-full flex-col items-center">
       <section className="mt-0 flex h-full w-full flex-col items-center justify-center bg-white drop-shadow dark:bg-black/60 md:mt-28 md:h-[55vh]">
         <div className="flex w-full max-w-7xl flex-col items-center justify-center gap-12 px-4 py-8 md:flex-row">
           <motion.div
             initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0}}
+            whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
             transition={{ duration: 0.15 }}
           >
-            <div className="w-[235px] inline-flex dark:hidden md:max-w-[335px] md:w-full">
+            <div className="inline-flex w-[235px] dark:hidden md:w-full md:max-w-[335px]">
               <Image src={profilePicLight} alt="foto de perfil" width={335} />
             </div>
 
-            <div className="hidden w-[235px] dark:inline-flex md:max-w-[335px] md:w-full">
+            <div className="hidden w-[235px] dark:inline-flex md:w-full md:max-w-[335px]">
               <Image src={profilePicDark} alt="foto de perfil" width={335} />
             </div>
           </motion.div>
 
-          <div className="flex max-w-2xl flex-col gap-5 pr-4 will-change-transform">
-            <motion.h3
-              className={`${outfit.className} text-center text-3xl font-semibold text-[#6e6ad2] md:text-left`}
-              initial={{ opacity: 0, x: -40 }}
-              whileInView={{ opacity: 1, x: 0 }}
-              viewport={{ once: true }}
-              transition={{ duration: 0.15, delay: baseDelay * 2 }}
-            >
-              Sobre mim
-            </motion.h3>
+          <LayoutGroup>
+            <div className="flex max-w-2xl flex-col gap-5 pr-4 will-change-transform">
+              <motion.h3
+                className={`${outfit.className} text-center text-3xl font-semibold text-[#6e6ad2] md:text-left`}
+                initial={{ opacity: 0, x: -40 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.15, delay: baseDelay * 2 }}
+              >
+                Sobre mim
+              </motion.h3>
 
-            <motion.p
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              transition={{ duration: 0.15, delay: baseDelay * 3 }}
-              className="text-justify text-sm dark:text-slate-50 md:text-base"
-            >
-              Meu nome é Gabriel Silva Feitosa, sou apaixonado por tecnologia e
-              programação. Atualmente estou me formando em <b>Ciência da
-              Computação.</b>
-            </motion.p>
-
-            <motion.p
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              transition={{ duration: 0.15, delay: baseDelay * 4 }}
-              className="text-justify text-sm dark:text-slate-50 md:text-base"
-            >
-              Sou <b>Desenvolvedor Web</b> com foco em <b>front-end.</b> Gosto de criar
-              interfaces e transforma-las em código, buscando sempre um
-              equilíbrio entre beleza e boa performance.
-            </motion.p>
-
-            <motion.p
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              transition={{ duration: 0.15, delay: baseDelay * 5 }}
-              className="text-justify text-sm dark:text-slate-50 md:text-base"
-            >
-              Além disso, estou expandindo meus conhecimentos para o mundo <b>back-end</b> e <b>mobile</b> por meio do <b>NodeJS</b> e <b>React Native.</b>
-            </motion.p>
-
-            <Link href="/Fullstack-Gabriel-Feitosa.pdf" target="blank">
-              <motion.button
+              <motion.p
                 initial={{ opacity: 0, y: 20 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true }}
-                transition={{ duration: 0.15, delay: baseDelay * 6 }}
-                className="flex w-32 flex-row justify-center gap-2 rounded bg-[#6e6ad2] px-4 py-2 text-slate-50 drop-shadow duration-200 hover:scale-105 hover:bg-[#6e6ad2]/80 hover:text-white hover:ease-in-out"
+                transition={{ duration: 0.15, delay: baseDelay * 3 }}
+                className="text-justify text-sm dark:text-slate-50 md:text-base"
               >
-                Currículo
-              </motion.button>
-            </Link>
-          </div>
+                Meu nome é Gabriel Silva Feitosa, sou apaixonado por tecnologia
+                e programação. Atualmente estou me formando em{" "}
+                <b>Ciência da Computação.</b>
+              </motion.p>
+
+              <motion.p
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.15, delay: baseDelay * 4 }}
+                className="text-justify text-sm dark:text-slate-50 md:text-base"
+              >
+                Sou <b>Desenvolvedor Web</b> com foco em <b>front-end.</b> Gosto
+                de criar interfaces e transforma-las em código, buscando sempre
+                um equilíbrio entre beleza e boa performance.
+              </motion.p>
+
+              <motion.p
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.15, delay: baseDelay * 5 }}
+                className="text-justify text-sm dark:text-slate-50 md:text-base"
+              >
+                Além disso, estou expandindo meus conhecimentos para o mundo{" "}
+                <b>back-end</b> e <b>mobile</b> por meio do <b>NodeJS</b> e{" "}
+                <b>React Native.</b>
+              </motion.p>
+
+              <Link href="/Fullstack-Gabriel-Feitosa.pdf" target="blank">
+                <motion.button
+                  initial={{ opacity: 0, y: 20 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  viewport={{ once: true }}
+                  transition={{ duration: 0.15, delay: baseDelay * 6 }}
+                  className="flex w-32 flex-row justify-center gap-2 rounded bg-[#6e6ad2] px-4 py-2 text-slate-50 drop-shadow duration-200 hover:scale-105 hover:bg-[#6e6ad2]/80 hover:text-white hover:ease-in-out"
+                >
+                  Currículo
+                </motion.button>
+              </Link>
+            </div>
+          </LayoutGroup>
         </div>
       </section>
 
