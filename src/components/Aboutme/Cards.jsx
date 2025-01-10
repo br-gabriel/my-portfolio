cb423cad70101afaf85ad01b5d0b9ceff84926bc
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-10 15:52:00 -0300

    fixed extra padding on x axis

--- src/components/Aboutme/Cards.jsx
+++ src/components/Aboutme/Cards.jsx
@@ -7,7 +7,7 @@ import mobileSVG from "../../../public/mobile.svg";
 export default function Cards() {
   return (
     <section
-      className="flex w-full snap-x snap-mandatory gap-4 overflow-x-scroll p-4 drop-shadow-md scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-800 md:items-center md:justify-center md:overflow-hidden"
+      className="flex w-full snap-x snap-mandatory gap-4 overflow-x-scroll py-4 drop-shadow-md scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-800 md:items-center md:justify-center md:overflow-hidden"
     >
       <div className="flex min-h-[120px] w-full min-w-[270px] max-w-[300px] snap-center items-center justify-center rounded-xl bg-white p-4 text-black dark:bg-black dark:text-white md:max-w-[360px]">
         <div className="flex items-center justify-center gap-6">
