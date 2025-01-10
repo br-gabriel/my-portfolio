31a4a22cffef5d9c11b339f45353aca1b7691acb
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-10 15:50:04 -0300

    updated about me section

--- src/components/Aboutme/Cards.jsx
+++ src/components/Aboutme/Cards.jsx
@@ -7,10 +7,9 @@ import mobileSVG from "../../../public/mobile.svg";
 export default function Cards() {
   return (
     <section
-      dra
-      className="flex w-full snap-x snap-mandatory gap-4 overflow-x-scroll p-4 drop-shadow-md will-change-transform scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-800 md:items-center md:justify-center md:overflow-hidden"
+      className="flex w-full snap-x snap-mandatory gap-4 overflow-x-scroll p-4 drop-shadow-md scrollbar-thin scrollbar-track-transparent scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-800 md:items-center md:justify-center md:overflow-hidden"
     >
-      <div className="flex min-h-[120px] w-full min-w-[270px] max-w-[300px] snap-center items-center justify-center rounded-md bg-white p-4 text-black dark:bg-black dark:text-white md:max-w-[360px]">
+      <div className="flex min-h-[120px] w-full min-w-[270px] max-w-[300px] snap-center items-center justify-center rounded-xl bg-white p-4 text-black dark:bg-black dark:text-white md:max-w-[360px]">
         <div className="flex items-center justify-center gap-6">
           <Image src={figmaSVG} alt="logo do Figma" width={50} />
           <span className="md:max-w-[150px]">
@@ -19,7 +18,7 @@ export default function Cards() {
         </div>
       </div>
 
-      <div className="flex min-h-[120px] w-full min-w-[270px] max-w-[300px] snap-center items-center justify-center rounded-md bg-white p-4 text-black dark:bg-black dark:text-white md:max-w-[360px]">
+      <div className="flex min-h-[120px] w-full min-w-[270px] max-w-[300px] snap-center items-center justify-center rounded-xl bg-white p-4 text-black dark:bg-black dark:text-white md:max-w-[360px]">
         <div className="flex items-center justify-center gap-6">
           <Image src={nextjsSVG} alt="logo do Next js" width={70} />
           <span className="md:max-w-[150px]">
@@ -28,7 +27,7 @@ export default function Cards() {
         </div>
       </div>
 
-      <div className="flex min-h-[120px] w-full min-w-[270px] max-w-[300px] snap-center items-center justify-center rounded-md bg-white p-4 text-black dark:bg-black dark:text-white md:max-w-[360px]">
+      <div className="flex min-h-[120px] w-full min-w-[270px] max-w-[300px] snap-center items-center justify-center rounded-xl bg-white p-4 text-black dark:bg-black dark:text-white md:max-w-[360px]">
         <div className="flex items-center justify-center gap-6">
           <Image src={mobileSVG} alt="Icone de smartphone" width={50} />
           <span className="md:max-w-[150px]">Sites responsivos</span>
--- src/components/Aboutme/index.jsx
+++ src/components/Aboutme/index.jsx
@@ -1,8 +1,8 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import profilePicLight from "../../../public/images/lightmode-avatar.png";
-import profilePicDark from "../../../public/images/darkmode-avatar.png";
+import profilePic from "../../../public/images/profilePic.png";
+import { IoLocationSharp } from "react-icons/io5";
 import { Outfit } from "next/font/google";
 import Cards from "./Cards";
 
@@ -14,42 +14,54 @@ export default function Aboutme() {
   const baseDelay = 0.2;
 
   return (
-    <div id="aboutMe" className="flex h-full w-full flex-col items-center">
-      <section className="mt-0 flex h-full w-full flex-col items-center justify-center bg-white drop-shadow dark:bg-black/60 md:mt-28 md:h-[55vh]">
-        <div className="flex w-full max-w-7xl flex-col items-center justify-center gap-12 px-4 py-8 md:flex-row">
-          <div>
-            <div className="inline-flex w-[235px] dark:hidden md:w-full md:max-w-[335px]">
-              <Image src={profilePicLight} alt="foto de perfil" width={335} />
-            </div>
-
-            <div className="hidden w-[235px] dark:inline-flex md:w-full md:max-w-[335px]">
-              <Image src={profilePicDark} alt="foto de perfil" width={335} />
+    <div id="aboutMe" className="flex h-full w-full flex-col items-center px-2">
+      <section className="mt-0 flex h-full w-full max-w-[69.5em] flex-col items-center justify-center rounded-xl bg-white px-5 drop-shadow dark:bg-black/60 md:mt-28">
+        <div className="flex w-full max-w-7xl flex-col items-center justify-center gap-12 px-4 py-8">
+          <div className="flex w-full items-center gap-4">
+            <Image src={profilePic} alt="foto de perfil" width={80} />
+            <div>
+              <h3
+                className={`${outfit.className} text-xl font-semibold text-gray-800 dark:text-white`}
+              >
+                Gabriel Feitosa
+              </h3>
+              <div className="flex items-center gap-1">
+                <IoLocationSharp className="text-[#6e6ad2]" />
+                <span className="text-sm font-light dark:text-white">
+                  Curitiba, PR - Brasil
+                </span>
+              </div>
             </div>
           </div>
 
-          <div className="flex max-w-2xl flex-col gap-5 pr-4 will-change-transform">
+          <div className="flex flex-col gap-5">
             <h3
-              className={`${outfit.className} text-center text-3xl font-semibold text-[#6e6ad2] md:text-left`}
+              className={`${outfit.className} text-center text-3xl font-semibold text-gray-800 dark:text-white md:text-left`}
             >
               Sobre mim
             </h3>
 
             <p className="text-justify text-sm dark:text-slate-50 md:text-base">
-              Meu nome é Gabriel Silva Feitosa, sou apaixonado por tecnologia e
-              programação. Atualmente estou me formando em{" "}
-              <b>Ciência da Computação.</b>
+              Meu nome é Gabriel, sou estudante de <b>Ciência da Computação</b>.
+              Tenho experiência em desenvolvimento web, com foco no{" "}
+              <b>front-end</b>, utilizando ferramentas como{" "}
+              <b>React, Tailwind e Styled Components</b> para criar interfaces
+              modernas e responsivas.
             </p>
 
             <p className="text-justify text-sm dark:text-slate-50 md:text-base">
-              Sou <b>Desenvolvedor Web</b> com foco em <b>front-end.</b> Gosto
-              de criar interfaces e transforma-las em código, buscando sempre um
-              equilíbrio entre beleza e boa performance.
+              Atualmente, estou expandindo minha atuação para o desenvolvimento{" "}
+              <b>full-stack</b>, aprimorando meus conhecimentos em{" "}
+              <b>
+                Node.js, React Native e bancos de dados como Postgres e MongoDB.
+              </b>{" "}
+              Além disso, tenho experiência com design e <b>SEO</b>, usando{" "}
+              <b>Figma</b> para projetos visuais.
             </p>
 
             <p className="text-justify text-sm dark:text-slate-50 md:text-base">
-              Além disso, estou expandindo meus conhecimentos para o mundo{" "}
-              <b>back-end</b> e <b>mobile</b> por meio do <b>NodeJS</b> e{" "}
-              <b>React Native.</b>
+              Busco constantemente aperfeiçoar minhas habilidades e contribuir
+              com <b>soluções criativas</b> que unam design atraente e funcionalidade.
             </p>
 
             <Link href="/Fullstack-Gabriel-Feitosa.pdf" target="blank">
