274559eecd5dbcd6385a4f3272e4248d66a5902b
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-15 23:00:28 -0300

    homepage update

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -81,7 +81,7 @@ export default function Homepage() {
           <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
           <span className="relative inline-flex h-3 w-3 rounded-full bg-green-500"></span>
         </span>
-        <p>Disponível para trabalhos freelance!</p>
+        <p>Disponível para trabalhos freelancer!</p>
       </motion.div>
     </div>
   );
